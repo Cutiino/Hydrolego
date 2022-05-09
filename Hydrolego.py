a0e31cd8df7bc36f60cbb70f0e6fdd3933c80ea6
Agustin Cutiño <agustin.cutino@gmail.com>
2022-05-09 00:23:37 -0400

    Se agrega función para traspasar los voltages de los sensore de EC y pH a sus variables correspondientes, se agrega función para obtener los promedios de las muestras tomadas en un periodo de tiempo definido y se muestran en la pantalla LCD estos promedios

--- Hydrolego.py
+++ Hydrolego.py
@@ -1,52 +1,108 @@
-from signal import signal, SIGTERM, SIGHUP, pause       #Librería para pantalla LCD
+from signal import signal, SIGTERM, SIGHUP, pause
+from telnetlib import EC                                #Librería para pantalla LCD
 from rpi_lcd import LCD                                 #Librería para Pantalla LCD
 from time import sleep                                  #Librería para delay
+from time import time                                   #Librería para obtener tiempo en Epoch
 from w1thermsensor import W1ThermSensor                 #Librería Sensor Temperatura db18b20
 import board, busio                                     #Librería para definir pines placa
 import adafruit_ads1x15.ads1015 as ADS                  #Librería adafruit para ADS
 from adafruit_ads1x15.analog_in import AnalogIn         #Librería adafruit para entrada Análoga
 
-lcd = LCD()                                             #Pantalla LCD
+#Pantalla LCD
+lcd = LCD()                                             #Define variable pantalla LCD                  
 
 def safe_exit(signum, frame):
     exit(1)
 
-def PantallaLCD(Temp, pH):                              #Función para escribir en pantalla LCD
-    try:
-        signal(SIGTERM, safe_exit)
-        signal(SIGHUP, safe_exit)
-        lcd.text("T°: " + Temp, 1)                      #Escribe en fila 1 Pantalla
-        lcd.text("pH:" + pH, 2)                         #Escribe en fila 2 Pantalla
-        pause()
-    except KeyboardInterrupt:
-        pass
-    finally:
-        lcd.clear()
+Pantalla = False
+def PantallaLCD(Temp, pH, EC):                          #Función para escribir en pantalla LCD
+    global Pantalla
+    if (Pantalla == True):
+        print("Mostrando datos en pantalla")
+        tiempoPantalla = tiempo
+        TempP = str(round(Temp, 2))
+        pHP = str(round(pH, 2))
+        ECP = str(round(EC, 2))
+        try:
+            signal(SIGTERM, safe_exit)
+            signal(SIGHUP, safe_exit)
+            lcd.text("T:" + TempP + " EC:" + ECP, 1)     #Escribe en fila 1 Pantalla            
+            lcd.text("pH:" + pHP, 2)                     #Escribe en fila 2 Pantalla
+            Pantalla = False
+        except:
+            print("Error en pantalla LCD")
+            lcd.clear()
 
 #Sensor Temperatura
 sensorT = W1ThermSensor()
 def Temperatura():                                      #Función que retorna la T° del agua
     TWater = sensorT.get_temperature()
-    #print("The temperature is %s celsius" % TWater)
+    #TWater = round(TWater, 2)                           #Redondea T° a la centesima
+    print("Temperatura: " + str(TWater))
     return TWater
 
 #Conversor ADC
 i2c = busio.I2C(board.SCL, board.SDA)                   #Crea bus I2C
 ads = ADS.ADS1015(i2c)                                  #Crea el objeto ADC usando I2C
-chan1 = AnalogIn(ads, ADS.P0)                           #Crea la entrada Sensor en canal 1
-chan2 = AnalogIn(ads, ADS.P1)                           #Crea la entrada Sensor en canal 2
+chan0 = AnalogIn(ads, ADS.P0)                           #Crea la entrada Sensor en canal 1
+chan1 = AnalogIn(ads, ADS.P1)                           #Crea la entrada Sensor en canal 2
+
+def ECSensor():
+    EcVoltage = chan0.voltage                           #Obtiene valor voltage sensor EC
+    #EcMuestra = round(EcMuestra, 2)                     #Redondea valor voltage a la centesima
+    print ("Channel 0: " + str(EcVoltage)  + "[V]")
+    ECMuestra = EcVoltage*7594.04 - 29.8676             #Ecuación para pasar de [V] a [us/cm]       
+    print ("EC: " + str(ECMuestra)  + "[us/cm]")
+    return ECMuestra
 
 #Sensor pH
 def pHSensor():
-    print ("Channel0: " + str(chan1.value) + "  "  + str(chan1.voltage))
+    pHVoltage = chan1.voltage                           #Obtine valor voltage sensor EC
+    #pHMuestra = round(pHMuestra, 2)                     #Redondea valor voltage a la centesima
+    print ("Channel 1: " + str(pHVoltage) + "[V]")
+    pHMuestra = pHVoltage*-5.859 + 15.99                #Ecuación para pasar voltage a pH
+    print ("pH: " + str(pHMuestra))
+    return pHMuestra
 
-def ECSensor():
-    print ("Channel1: " + str(chan2.value) + "  "  + str(chan2.voltage))
+#Promedios
+borrarProm = True
+def promSensores(Temp, pH, EC, tiempoProm):             #Función para adquirir promedio datos en un tiempo
+    global muestras, tiempoSensores, promT              #Defincion variables globales
+    global promEC, prompH, Pantalla, borrarProm         #Defincion variables globales
+    promedioDatos = [0, 0, 0]                           #Creación lista para retornar promedios valores
+    if (borrarProm == True):                            #Inicialización de valores prom
+        muestras = 0
+        promT = 0
+        promEC = 0
+        prompH = 0
+        borrarProm = False
+        tiempoSensores = tiempoProm
+    Intervalo = 15                                      #Variable de tiempo para obtener las muestras en seg
+    if (tiempoProm - tiempoSensores < 15):              
+        promT = promT + Temp                            #Suma cada muestra de Temperatura
+        promEC = promEC + EC                            #Suma cada muestra de EC
+        prompH = prompH + pH                            #Suma cada muestra de pH
+        muestras = muestras + 1                         #Lleva la cantidad de muestras que se han obtenido
+    else:
+        promT = promT/muestras                          #Calcula promedio Temperatura
+        prompH = prompH/muestras                        #Calcula promedio pH
+        promEC = promEC/muestras                        #Calcula promedio EC
+        borrarProm = True                               #Habilita que variables se reinicien para siguiente intervalo
+        Pantalla = True                                 #Habilita datos se muestren en Pantalla
+        promedioDatos [0] = promT                       #Se meten en una lista promedio Temperatura
+        promedioDatos [1] = prompH                      #Se meten en una lista promedio pH
+        promedioDatos [2] = promEC                      #Se meten en una lista promedio EC
+        print("\nPromedio T: " + str(promT))
+        print("Promedio pH: " + str(prompH))
+        print("Promedio EC: " + str(promEC) + "\n")
+    return promedioDatos                                #Retorna en una lista los promedios
 
 while True:                                             #Bucle
-    DatoT = str(Temperatura())                          #Adquiere dato T°
-    print("Temperatura: " + DatoT)                      #Imprime dato T°
-    pHSensor()                              
-    ECSensor()
-    #PantallaLCD(Temperatura, "10")                     #Muestra en pantalla los datos de los sensores
-
+    tiempo = time()
+    datoT = Temperatura()                               #Adquiere dato T°
+    datoEC = ECSensor()                                 #Adquiere dato EC
+    datopH = pHSensor()                                 #Adquiere dato pH
+    media = promSensores(datoT, datopH, datoEC, tiempo) #Saca promedio datos cada 15s
+    #media = [Temperatura, pH, EC]
+    PantallaLCD(media[0], media[1], media[2])           #Muestra en pantalla los datos de los sensores
+    
---
+++ readme.txt
@@ -0,0 +1,25 @@
+Sensor Electroconductividad
+- Si se descubrio que el electrodo negro ha disminuido su rendimiento,
+se puede usar etanol y agua desionizada para lavar lamina de platino.
+
+Tabla Calibración
+V | EC
+0,19 | 1413 [us/cm]
+1,7| 12880 [us/cm] -> 12,88 [ms/cm]
+
+m = 12880 - 1413/1,7 - 0,19 = 11467/1,51 = 7594,0397
+
+y - 1413 = 7594,04(x - 0.19) = y=7594.04x - 29.8676
+
+Ecuación de la recta
+
+
+Sensor pH
+V | pH
+2,048 | 4
+1,536 | 7
+
+m = 7 - 4/ 1,536 - 2,048 = 3/-0.512 = -5.859
+
+y - 4 = -5.859(x - 2.048)
+y = -5.859x + 15.999
